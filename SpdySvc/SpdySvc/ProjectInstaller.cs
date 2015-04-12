6e5473f75a68e2106fc59614fd8e6f74395b2a1c
rahul <rahul@bglobalsourcing.com>
2015-04-12 13:34:54 +0600

    commit

---
+++ SpdySvc/SpdySvc/ProjectInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SpdySvc
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            //this.serviceProcessInstaller1.Account = ServiceAccount.User;
+            //this.serviceProcessInstaller1.Username = "rahul";
+            //this.serviceProcessInstaller1.Password = "password"; // password;
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SpdySvc/SpdySvc/Service1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Configuration;
+using DBManager;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+namespace SpdySvc
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+         
+        }
+        private Timer Schedular;
+        protected override void OnStart(string[] args)
+        {
+            this.ScheduleService();
+        }
+        public void ScheduleService()
+        {
+            try
+            {
+                Schedular = new Timer(new TimerCallback(SchedularCallback));
+                string connectionstring = ConfigurationManager.ConnectionStrings["ConnStringDb"].ToString();
+                MediaConnection.Instance().ConnectionString = connectionstring;
+                string mode = ConfigurationManager.AppSettings["Mode"];
+                
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse(ConfigurationManager.AppSettings["ScheduledTime"]);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                var obj = new DBMContext();
+                obj.SendEmail(new EMAIL { Email = "das063020@gmail.com", Message = "Hi", Message_Code = "", MessageSubject = "Hi", ContactNo = "" });
+                obj.Dispose();
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                Schedular.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+          
+
+                //Stop the Windows Service.
+                using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+        private void SchedularCallback(object e)
+        {
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            this.Schedular.Dispose();
+        }
+
+
+    }
+}
